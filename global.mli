b09a0825d270095dfb3cff96384c4fbb1ddf2715
Bertrand Bonnefoy-Claudet <bertrandbc@gmail.com>
2014-08-01 16:45:02 +0200

    Interface for Global

---
+++ global.mli
@@ -0,0 +1,16 @@
+(** Global state. *)
+
+(** Type of a global state. *)
+type t = Heap.t * As.t
+
+(** Test whether two global states are equal. *)
+val equal : t -> t -> bool
+
+(** Global state with empty heap and empty activity stack. *)
+val bot : t
+
+(** Join of two global states. *)
+val join : t -> t -> t
+
+(** Output a string representation of a global state. *)
+val to_string : t -> string
